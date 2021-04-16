04c4202b37feb50476f3c0fba526e919e633e64a
daveyrojo <d.vincenteldridge@gmail.com>
2021-04-16 00:54:12 -0400

    created routes on app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Images from "./images.json"
 import NavBar from "./components/Nav";
 import Footer from "./components/Footer";
 import AboutMe from "./pages/AboutMe";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import ModalCard from "./components/ModalCard";
 
 function App() {
   return (
     <Router>
       <NavBar />
-        <Home /> 
-        <AboutMe images={Images} />
+      <ModalCard />
+      <Switch>
+        <Route to="/" exact path>
+          <Home />
+        </Route>
+        <Route to="/aboutme" exact path>
+          <AboutMe images={Images} />
+        </Route>
+      </Switch>
       <Footer />
     </Router>
   );
