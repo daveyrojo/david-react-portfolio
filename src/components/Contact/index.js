81480023747e736b66fffaec48d166789d977d52
daveyrojo <d.vincenteldridge@gmail.com>
2021-04-16 22:52:26 -0400

    fixed contact links

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 
 function Contact() {
-
-    return(
-
-        <div>
-            <a style={{color: "primary"}} href='https://www.linkedin.com/in/david-vincent-eldridge/'>
-                LinkedIn.
-                </a>
-            <a style={{color: "primary"}} href='https://github.com/daveyrojo'>
-                GitHub.
-                </a>
-            <a style={{color: "primary"}}href="mailto: d.vincenteldridge@gmail.com">
-                Email.
-            </a>
-        </div>
-
+    
+    return (
+      <div style={{ textAlign: "center" }}>
+        <a
+          style={{ color: "primary", marginRight: 45 }}
+          href="https://www.linkedin.com/in/david-vincent-eldridge/"
+        >
+          LinkedIn.
+        </a>
+        <a
+          style={{ color: "green", marginRight: 45 }}
+          href="https://github.com/daveyrojo"
+        >
+          GitHub.
+        </a>
+        <a
+          style={{ color: "purple", marginRight: 45 }}
+          href="mailto: d.vincenteldridge@gmail.com"
+        >
+          E-mail.
+        </a>
+      </div>
     );
 }
 
